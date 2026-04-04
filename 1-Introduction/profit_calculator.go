0a0f7b1cfe816343b639ab0d41c41eaa710d46ed
Yussuki <uehara_thiago@hotmail.com>
2026-04-03 21:05:06 -0300

    Adding formatting, error handling and file writing

--- 1-Introduction/profit_calculator.go
+++ 1-Introduction/profit_calculator.go
@@ -1,30 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
 func main() {
 	var revenue float64
 	var expenses float64
 	var taxRate float64
 
-	revenue = getInput("Revenue: ")
-	expenses = getInput("Expenses: ")
-	taxRate = getInput("Tax Rate: ")
+	revenue, err := getInput("Revenue: ")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	expenses, err = getInput("Expenses: ")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	taxRate, err = getInput("Tax Rate: ")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	ebt, profit, ratio := calculate(revenue, expenses, taxRate)
 
-	fmt.Println("EBT:", ebt)
-	fmt.Println("Profit:", profit)
-	fmt.Println("Ratio:", ratio)
+	fmt.Printf("EBT: %.1f\n", ebt)
+	fmt.Printf("Profit: %.1f\n", profit)
+	fmt.Printf("Ratio: %.3f\n", ratio)
 
+	os.WriteFile("profit.txt", []byte(fmt.Sprintf("EBT: %.1f\nProfit: %.1f\nRatio: %.3f\n", ebt, profit, ratio)), 0644)
 }
 
-func getInput(inputText string) (userInput float64) {
+func getInput(inputText string) (float64, error) {
+	var userInput float64
 
 	fmt.Print(inputText)
-	fmt.Scan(&userInput)
 
-	return userInput
+	fmt.Scan(&userInput)
+	if userInput <= 0 {
+		return 0, errors.New("Invalid value provided. Make sure it is a positive value.")
+	}
+
+	return userInput, nil
 }
 
 func calculate(revenue, expenses, taxRate float64) (ebt float64, profit float64, ratio float64) {
